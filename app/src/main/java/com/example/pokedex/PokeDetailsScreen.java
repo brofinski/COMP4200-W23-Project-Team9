420fe6c7a4a247d599bb7eb71406bdb8f70ef86d
Chris Rafinski <rafinsk@uwindsor.ca>
2023-03-27 12:52:30 -0400

    Added ImageView and TextView declarations to the PokeDetailsScreen.java so we can connect information to them later

--- app/src/main/java/com/example/pokedex/PokeDetailsScreen.java
+++ app/src/main/java/com/example/pokedex/PokeDetailsScreen.java
@@ -3,12 +3,28 @@ package com.example.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PokeDetailsScreen extends AppCompatActivity {
+
+    ImageView iv_pokemonAvatar;
+    TextView tv_pokemonNumber, tv_pokemonName, tv_pokemonType, tv_pokemonHeight,
+        tv_pokemonWeight, tv_pokemonDescription;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poke_details_screen);
+
+        iv_pokemonAvatar = findViewById(R.id.imageView_pokemonAvatar);
+        tv_pokemonNumber = findViewById(R.id.textView_pokeNumberDetailsScreen);
+        tv_pokemonName = findViewById(R.id.textView_pokeNameDetailsScreen);
+        tv_pokemonType = findViewById(R.id.textView_pokeTypeDetailsScreen);
+        tv_pokemonHeight = findViewById(R.id.textView_pokeHeightDetailsScreen);
+        tv_pokemonWeight = findViewById(R.id.textView_pokeWeightDetailsScreen);
+        tv_pokemonDescription = findViewById(R.id.textView_pokeDescriptionDetailsScreen);
     }
 }
